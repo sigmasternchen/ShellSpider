bc76c14000fb81236fe1a272102c37181c4427eb
overflowerror <mail@overflowerror.com>
2017-11-19 02:53:30 +0100

    fix character encoding

--- misc.sh
+++ misc.sh
@@ -5,5 +5,5 @@ urlencode() {
 }
 
 urldecode() {
-	python -c "import urllib, sys; print urllib.unquote_plus(sys.argv[1]).decode('utf8')"  "$1"
+	python -c "import urllib, sys; print urllib.unquote_plus(sys.argv[1])"  "$1"
 }
